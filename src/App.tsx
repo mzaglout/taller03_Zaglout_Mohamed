384a7ff9e44cd19d405614ec296069408ac3ebaf
mzaglout <mzaglout@cuc.edu.co>
2024-09-23 09:30:39 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import miLogo from "./assets/png-transparent-wolf-logo-symbol-thumbnail.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="Logo de React" />
+        <h1>Adios Mundo</h1>
+        <p>Este es el primer taller de buenas practicas segundo corte</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
